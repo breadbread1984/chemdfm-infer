24498ef74f10b79a1b9c31f55bef0790ed21e8d4
breadbread1984 <breadbread1984@163.com>
2024-04-18 16:28:20 +0800

    update

--- models.py
+++ models.py
@@ -3,7 +3,7 @@
 from typing import List
 import torch
 from huggingface_hub import login
-from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper
+from transformers import AutoTokenizer, AutoModelForCausalLM, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper, TopKLogitsWarper, RepetitionPenaltyLogitsProcessor
 from langchain.llms.base import LLM
 
 class ChemDFM(LLM):
@@ -20,10 +20,12 @@ class ChemDFM(LLM):
   def _call(self, prompt, stop = None, run_manager = None, **kwargs):
     logits_processor = LogitsProcessorList()
     logits_processor.append(TemperatureLogitsWarper(0.9))
+    logits_processor.append(TopKLogitsWarper(20))
     logits_processor.append(TopPLogitsWarper(0.9))
+    logits_processor.append(RepetitionPenaltyLogitsProcessor(1.05))
     inputs = self.tokenizer(prompt, return_tensors = 'pt')
     inputs = inputs.to(torch.device(self.model.device))
-    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = True, use_cache = True, return_dict_in_generate = True, max_new_tokens = 2048)
+    outputs = self.model.generate(**inputs, logits_processor = logits_processor, do_sample = False, use_cache = True, return_dict_in_generate = True, max_new_tokens = 2048)
     input_ids = outputs.sequences
     outputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens = True)
     response = outputs[0][len(prompt):]
